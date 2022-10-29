918bd59fc96958c9b94e0e0f62f349de506a4875
DaveDeDave <76876642+DaveDeDave@users.noreply.github.com>
2022-10-29 11:13:25 +0200

    Added initial files

---
+++ packages/advice-generator-app/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{vue,js}"],
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
